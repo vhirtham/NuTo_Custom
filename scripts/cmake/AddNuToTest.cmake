7150eb2543969cc93db108ea8444f225be72cadc
Volker Hirthammer <volker.hirthammer@gmail.com>
2018-02-12 10:55:15 +0100

    Add basic setup and first test

---
+++ scripts/cmake/AddNuToTest.cmake
@@ -0,0 +1,50 @@
+#[==[
+This function does all the necessary steps to set up a unit test
+#]==]
+
+function(addNuToTest TestName)
+    ### Add prefix "Test_" to avoid target naming conflicts (for example with benchmarks or other GDL targets)
+    #set(TestName "Test_${TestName}")
+
+    ### Find single object targets
+    #if(ARGN)
+    #    foreach(filename ${ARGN})
+    #        singleSourceTargetName(${CMAKE_SOURCE_DIR}/src/${filename} target)
+    #        set(AdditionalObjects
+    #            "${AdditionalObjects};$<TARGET_OBJECTS:${target}>")
+    #    endforeach()
+    #endif()
+
+    ### Create executable
+    add_executable(${TestName}
+        "${TestName}.cpp"
+        #${AdditionalObjects}
+        )
+
+    ### Link necessary libs
+    add_dependencies(${TestName} NuTo)
+    target_link_Libraries(${TestName}
+        Boost::unit_test_framework
+        NuToBase
+        NuToMath
+        NuToMechanics)
+
+    ### Add source directory
+    target_include_directories(${TestName}
+        PUBLIC
+            ${PROJECT_SOURCE_DIR}/src)
+
+    ### Add necessary definitions
+    target_compile_definitions(${TestName}
+        PRIVATE
+            -DBOOST_TEST_MODULE=${TestName}
+            -DBOOST_TEST_DYN_LINK)
+
+    ### Create Test
+    string(REPLACE "${CMAKE_SOURCE_DIR}/tests/" ""
+        relpath ${CMAKE_CURRENT_SOURCE_DIR})
+    string(REPLACE "/" "::"
+        TestPrefix ${relpath})
+    add_test("${TestPrefix}::${TestName}" ${TestName})
+
+endfunction()
