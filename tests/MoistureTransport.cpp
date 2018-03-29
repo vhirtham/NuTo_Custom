702d9a889395087899f40dad807e61885bc04455
Volker Hirthammer <volker.hirthammer@gmail.com>
2018-03-29 09:59:14 +0200

    Reenable some tests

--- tests/MoistureTransport.cpp
+++ tests/MoistureTransport.cpp
@@ -117,33 +117,33 @@ void IntegrationTest(int numElements, double delta_t, double t_final, double lWV
     }
 }
 
-////! @brief Checks if the submatrices and subvectors for the two phase system are sized correctly for different
-////! interpolation types.
-// BOOST_AUTO_TEST_CASE(InterpolationCombinations)
-//{
-//    for (int i = 1; i < 4; ++i)
-//        for (int j = 1; j < 4; ++j)
-//        {
-//            MoistureTransportTest<1, MTCConst<1>, MTCConst<0>, MTCConst<1>, MTCConst<2, 10>> MTT;
-//            MTT.CreateUnitMesh(1, InterpolationTrussLobatto(i), InterpolationTrussLobatto(j));
-//            MTT.Gradient();
-//            MTT.Stiffness();
-//            MTT.Damping();
-//        }
-//}
+//! @brief Checks if the submatrices and subvectors for the two phase system are sized correctly for different
+//! interpolation types.
+BOOST_AUTO_TEST_CASE(InterpolationCombinations)
+{
+    for (int i = 1; i < 4; ++i)
+        for (int j = 1; j < 4; ++j)
+        {
+            MoistureTransportTest<1, MTCConst<1>, MTCConst<0>, MTCConst<1>, MTCConst<2, 10>> MTT;
+            MTT.CreateUnitMesh(1, InterpolationTrussLobatto(i), InterpolationTrussLobatto(j));
+            MTT.Gradient();
+            MTT.Stiffness();
+            MTT.Damping();
+        }
+}
 
 
-////! @brief This test checks if an exception is thrown when the water volume fraction equals the pore volume fraction.
-////! This is important because in this case there is no volume left for the gas phase which will lead to undefined
-////! behaviour of the gas phase transport equation.
-// BOOST_AUTO_TEST_CASE(PoresCompletlyFilledWithWater)
-//{
-//    MoistureTransportTest<1, MTCConst<1>, MTCConst<0>, MTCConst<1>, MTCConst<2, 10>> MTT(1, 1, 0.2);
-//    MTT.CreateUnitMesh(1, InterpolationTrussLobatto(1), InterpolationTrussLobatto(1));
-//    BOOST_CHECK_THROW(MTT.Gradient(), Exception);
-//    BOOST_CHECK_THROW(MTT.Stiffness(), Exception);
-//    BOOST_CHECK_THROW(MTT.Damping(), Exception);
-//}
+//! @brief This test checks if an exception is thrown when the water volume fraction equals the pore volume fraction.
+//! This is important because in this case there is no volume left for the gas phase which will lead to undefined
+//! behaviour of the gas phase transport equation.
+BOOST_AUTO_TEST_CASE(PoresCompletlyFilledWithWater)
+{
+    MoistureTransportTest<1, MTCConst<1>, MTCConst<0>, MTCConst<1>, MTCConst<2, 10>> MTT(1, 1, 0.2);
+    MTT.CreateUnitMesh(1, InterpolationTrussLobatto(1), InterpolationTrussLobatto(1));
+    BOOST_CHECK_THROW(MTT.Gradient(), Exception);
+    BOOST_CHECK_THROW(MTT.Stiffness(), Exception);
+    BOOST_CHECK_THROW(MTT.Damping(), Exception);
+}
 
 BOOST_AUTO_TEST_CASE(Integrationtest)
 {
